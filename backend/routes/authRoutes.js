cf277b5d259555d54f808ec4424283a48389fc1d
neehaanzum <neehaanzum95@gmail.com>
2026-01-28 17:52:19 +0530

    added SpringBoard

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  verifyOTP,
+  resetPassword
+} = require("../controllers/authController");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOTP);
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
